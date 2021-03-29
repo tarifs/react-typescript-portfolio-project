bea44331e93620b830cab49a4fb2b3bcbd9aae1f
Tarif hossain <tarif@namespaceit.com>
2021-03-29 11:32:34 +0600

    user input code bind

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -27,11 +27,11 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin()],
+      plugins: [unpkgPathPlugin(input)],
       define: {
-        'process.env.NODE_ENV' : '"production"',
-        global: 'window'
-      }
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
     });
 
     // console.log(result);
