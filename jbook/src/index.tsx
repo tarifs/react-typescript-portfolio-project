68a58f8673613cb7e8af0a31a66fb982469f7ada
Tarif hossain <tarif@namespaceit.com>
2021-03-31 11:30:05 +0600

    Refactoring to Multiple Plugins

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from 'esbuild-wasm';
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
 const App = () => {
   const ref = useRef<any>();
@@ -27,7 +28,10 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window',
@@ -40,16 +44,16 @@ const App = () => {
   };
 
   return (
-    <div>
-      <textarea
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      ></textarea>
       <div>
-        <button onClick={onClick}>Submit</button>
+      <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
       </div>
-      <pre>{code}</pre>
-    </div>
   );
 };
 
